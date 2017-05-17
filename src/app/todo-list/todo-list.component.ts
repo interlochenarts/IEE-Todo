47be75f3e47642c687c5e3e0435d4f8dde6d17e7
Michael Smith <smithmd@interlochen.org>
2017-05-17 16:22:24 -0400

    fixed sorting

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Todo} from '../todo';
+// const Visualforce = require('../lib/VFRemote');
+
+declare var Visualforce: any;
 
 @Component({
   selector: 'app-todo-list',
@@ -7,41 +10,11 @@ import {Todo} from '../todo';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
-  // todos: Todo[];
-  todos: Todo[] = [
-    {
-      isComplete: true,
-      personAssigned: 'Daughter Christmas',
-      description: 'Finish Application',
-      dueDate: '2012-04-23T18:25:43.511Z'
-    },
-    {
-      isComplete: false,
-      personAssigned: 'Daughter Christmas',
-      description: 'Pay Deposit',
-      dueDate: '2013-04-23T18:25:43.511Z'
-    },
-    {
-      isComplete: true,
-      personAssigned: 'Daughter Christmas',
-      description: 'Finish Application',
-      dueDate: '2014-04-23T18:25:43.511Z'
-    },
-    {
-      isComplete: true,
-      personAssigned: 'Daughter Christmas',
-      description: 'Finish Application',
-      dueDate: '2015-04-23T18:25:43.511Z'
-    },
-    {
-      isComplete: false,
-      personAssigned: 'Daughter Christmas',
-      description: 'Finish Application',
-      dueDate: '2016-04-23T18:25:43.511Z'
-    }
-  ];
-  isLoading = false;
-  showComplete = true;
+  todos: Todo[];
+  isLoading = true;
+  showComplete = false;
+  sortColumn: string;
+  sortAscending = false;
 
   constructor() {
   }
@@ -51,16 +24,80 @@ export class TodoListComponent implements OnInit {
   }
 
   loadData() {
-    // Visualforce.remoting.Manager.invokeAction(
-    //   '{!$RemoteAction.}',
-    //   (result, event) => {
-    //     if (result) {
-    //       this.isLoading = false;
-    //       this.todos = result;
-    //     }
-    //   }, {
-    //     buffer: false
-    //   }
-    // );
+    Visualforce.remoting.Manager.invokeAction(
+      'IEE_TodoViewController.getTodoList',
+      (result, event) => {
+        if (result) {
+          this.isLoading = false;
+          this.todos = result;
+        }
+      }, {
+        buffer: false
+      }
+    );
+  }
+
+  getSortDirection(col: string) {
+    return this.sortColumn === col ? !this.sortAscending : false;
+  }
+
+  sortByComplete(): void {
+    this.sortAscending = this.getSortDirection('complete');
+    this.todos.sort((a, b) => {
+      if (this.sortAscending) {
+        return (a.isComplete === b.isComplete) ? 0 : a.isComplete ? -1 : 1;
+      } else {
+        return (a.isComplete === b.isComplete) ? 0 : a.isComplete ? 1 : -1;
+      }
+    });
+    this.sortColumn = 'complete';
+  }
+
+  sortByName(): void {
+    this.sortAscending = this.getSortDirection('name');
+    this.todos.sort((a, b) => {
+      if (this.sortAscending) {
+        return (a.personAssigned === b.personAssigned) ? 0 : a.personAssigned > b.personAssigned ? -1 : 1;
+      } else {
+        return (a.personAssigned === b.personAssigned) ? 0 : a.personAssigned > b.personAssigned ? 1 : -1;
+      }
+    });
+    this.sortColumn = 'name';
+  }
+
+  sortByDescription(): void {
+    this.sortAscending = this.getSortDirection('description');
+    this.todos.sort((a, b) => {
+      if (this.sortAscending) {
+        return (a.description === b.description) ? 0 : a.description > b.description ? -1 : 1;
+      } else {
+        return (a.description === b.description) ? 0 : a.description > b.description ? 1 : -1;
+      }
+    });
+    this.sortColumn = 'description';
+  }
+
+  sortByDueDate(): void {
+    this.sortAscending = this.getSortDirection('dueDate');
+    this.todos.sort((a, b) => {
+      if (this.sortAscending) {
+        return (a.dueDate === b.dueDate) ? 0 : a.dueDate > b.dueDate ? -1 : 1;
+      } else {
+        return (a.dueDate === b.dueDate) ? 0 : a.dueDate > b.dueDate ? 1 : -1;
+      }
+    });
+    this.sortColumn = 'dueDate';
+  }
+
+  getSortDirectionClass(col: string) {
+    let sortClass: string;
+
+    if (this.sortColumn === col) {
+      sortClass = this.sortAscending ? 'fa-caret-up' : 'fa-caret-down';
+    } else {
+      sortClass = '';
+    }
+
+    return sortClass;
   }
 }

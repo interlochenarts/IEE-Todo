d24787dcef7db18cb51fc57b31c40c851ffeb5c3
Mike Smith <smithmd@interlochen.org>
2018-10-25 15:12:32 -0400

    updating to angular 7 to fix security issues

--- src/app/show-complete-tasks.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowCompleteTasksPipe } from './show-complete-tasks.pipe';
-
-describe('ShowCompleteTasksPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShowCompleteTasksPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
